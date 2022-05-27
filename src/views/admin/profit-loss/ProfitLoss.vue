c5d619aa9c036e3fa986800e8cc072f63435368a
ajcastro <ajcastro29@gmail.com>
2022-05-27 11:14:58 +0800

    Fix

--- src/views/admin/profit-loss/ProfitLoss.vue
+++ src/views/admin/profit-loss/ProfitLoss.vue
@@ -1,56 +1,72 @@
 <template>
-    <div class="w-100">
+  <div class="w-100">
+    <b-row>
+      <b-col
+        cols="12"
+        md="6"
+      >
+        <h1>P/L</h1>
+
+        <div />
+      </b-col>
+    </b-row>
+    <b-row cols="12">
+      <b-col
+        cols="12"
+        md="6"
+      >
+        <label for="dateFrom">Date From</label>
+        <b-form-datepicker
+          id="dateFrom"
+          v-model="dateFrom"
+          :date-format-options="{ year: 'numeric', month: 'short', day: '2-digit', weekday: 'short' }"
+        />
+      </b-col>
+      <b-col
+        cols="12"
+        md="6"
+      >
+        <label for="dateTo">Date To</label>
+        <b-form-datepicker
+          id="dateTo"
+          v-model="dateTo"
+          :date-format-options="{ year: 'numeric', month: 'short', day: '2-digit', weekday: 'short' }"
+        />
+      </b-col>
+    </b-row>
+    <b-row
+      cols="12"
+      class="my-2"
+    >
+      <b-col
+        cols="12"
+        xl="12"
+      >
+        <profit-loss-chart />
+      </b-col>
+      <b-col
+        cols="12"
+        lg="12"
+        xl="12"
+        class="mt-3"
+      >
         <b-row>
-            <b-col cols="12" md="6">
-                <h1>P/L</h1>
-                <v-select
-                    placeholder="- All Websites -"
-                    :value="selectedWebsiteId"
-                    :dir="$store.state.appConfig.isRTL ? 'rtl' : 'ltr'"
-                    :options="websites"
-                    :reduce="(item) => item.id"
-                    label="code"
-                    class="website-selector my-2"
-                    @input="onSelectWebsite"
-                />
-                <div>
-                </div>
-            </b-col>
+          <b-col
+            cols="12"
+            lg="6"
+          >
+            <branch-chart />
+          </b-col>
+          <b-col
+            cols="12"
+            lg="6"
+          >
+            <provider-chart />
+          </b-col>
         </b-row>
-        <b-row cols="12">
-            <b-col cols="12" md="6">
-                <label for="dateFrom">Date From</label>
-                <b-form-datepicker
-                    id="dateFrom"
-                    :date-format-options="{ year: 'numeric', month: 'short', day: '2-digit', weekday: 'short' }"
-                    v-model="dateFrom"
-                />
-            </b-col>
-            <b-col cols="12" md="6">
-                <label for="dateTo">Date From</label>
-                <b-form-datepicker
-                    id="dateTo"
-                    :date-format-options="{ year: 'numeric', month: 'short', day: '2-digit', weekday: 'short' }"
-                    v-model="dateTo"
-                />
-            </b-col>
-        </b-row>
-        <b-row cols="12" class="my-2">
-            <b-col cols="12" xl="12">
-                <profit-loss-chart />
-            </b-col>
-            <b-col cols="12" lg="12" xl="12" class="mt-3">
-                <b-row>
-                    <b-col cols="12" lg="6">
-                        <branch-chart />
-                    </b-col>
-                    <b-col cols="12" lg="6">
-                        <provider-chart />
-                    </b-col>
-                </b-row>
-            </b-col>
-        </b-row>
-    </div>
+      </b-col>
+    </b-row>
+  </div>
 </template>
 
 <script>
@@ -61,31 +77,31 @@ import BranchChart from '@/views/admin/profit-loss/components/BranchChart.vue'
 import ProviderChart from '@/views/admin/profit-loss/components/ProviderChart.vue'
 
 export default {
-    name: 'ProfitLoss',
+  name: 'ProfitLoss',
 
-    components: {
-        BRow,
-        BCol,
-        vSelect,
-        BFormDatepicker,
-        ProfitLossChart,
-        BranchChart,
-        ProviderChart,
-    },
+  components: {
+    BRow,
+    BCol,
+    vSelect,
+    BFormDatepicker,
+    ProfitLossChart,
+    BranchChart,
+    ProviderChart,
+  },
 
-    data () {
-        return {
-            selectedWebsiteId: null,
-            websites: [],
-            dateFrom: null,
-            dateTo: null
-        }
-    },
-
-    methods: {
-        onSelectWebsite () {
-
-        }
+  data() {
+    return {
+      selectedWebsiteId: null,
+      websites: [],
+      dateFrom: null,
+      dateTo: null,
     }
+  },
+
+  methods: {
+    onSelectWebsite() {
+
+    },
+  },
 }
 </script>
